334c8d0fcb57e98294d7bee7c32b22e2bf0e39b9
Daniel Louchansky <bekasik@gmail.com>
2016-02-15 00:16:34 +0200

    removed unnecessary code

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Using Karma + Jasmine for unit tests.
 Currently all primitive types and Array, Number, Boolean, String, Object objects are supported
 
-### Questions to be clarified in lecture:
+### To be clarified
 * constructing objects using "prototype" property
 * constructing objects using "new" keyword
 * if each function is an object then they're not stored in stack and buffer overflow is impossible?
--- src/myStringify.js
+++ src/myStringify.js
@@ -1,85 +1,74 @@
 define(function () {
 
-        function lastComma(index, array) {
-            if (index + 1 != array.length) {
-                return ",";
+    function lastComma(index, array) {
+        if (index + 1 != array.length) {
+            return ",";
+        }
+
+        return "";
+    }
+
+    function stringifyObjectProperty(source) {
+        return function (accumulator, current, index, array) {
+            var value = stringifySomething(source[current]);
+            var key = stringifySomething(current);
+
+            if (value === "") {
+                return accumulator;
             }
 
+            return accumulator + key + ":" + value + lastComma(index, array);
+        }
+    }
+
+    function stringifyArrayElement(source) {
+        return function (accumulator, current, index, array) {
+            var value = stringifySomething(source[current]);
+
+            if (value === "") {
+                return accumulator;
+            }
+
+            return accumulator + value + lastComma(index, array);
+        }
+    }
+
+    function stringifySomething(source) {
+        if (typeof source == "undefined") {
             return "";
         }
 
-        function stringifyObjectProperty(source) {
-            return function (accumulator, current, index, array) {
-                var value = stringifySomething(source[current]);
-                var key = stringifySomething(current);
-
-                if (value === "") {
-                    return accumulator;
-                }
-
-                return accumulator + key + ":" + value + lastComma(index, array);
-            }
+        if (source == null) {
+            return 'null';
         }
 
-        function stringifyArrayElement(source) {
-            return function (accumulator, current, index, array) {
-                var value = stringifySomething(source[current]);
-
-                if (value === "") {
-                    return accumulator;
-                }
-
-                return accumulator + value + lastComma(index, array);
-            }
+        if (typeof source == "string" || source instanceof String) {
+            return '"' + source + '"';
         }
 
-        function stringifySomething(source) {
-            if (typeof source == "undefined") {
-                return "";
-            }
-
-            if (source == null) {
-                return null;
-            }
-
-            if (typeof source == "string" || source instanceof String) {
-                return '"' + source + '"';
-            }
-
-            if (typeof source == "boolean" || typeof source == "number" || source instanceof Boolean || source instanceof Number) {
-                return source;
-            }
-
-            var enumeratedMethods = Object.keys(source);
-
-            if (Array.isArray(source)) {
-                return "[" + enumeratedMethods.reduce(stringifyArrayElement(source), "") + "]";
-            }
-
-            return "{" + enumeratedMethods.reduce(stringifyObjectProperty(source), "") + "}";
+        if (typeof source == "boolean" || typeof source == "number" || source instanceof Boolean || source instanceof Number) {
+            return source;
         }
 
+        var enumeratedMethods = Object.keys(source);
 
-        return function (obj) {
+        if (Array.isArray(source)) {
+            return "[" + enumeratedMethods.reduce(stringifyArrayElement(source), "") + "]";
+        }
 
-            if (typeof obj == "undefined") {
-                return undefined;
-            }
-
-            if (obj == null) {
-                return 'null';
-            }
-
-            if (typeof obj == "boolean" || typeof obj == "number") {
-                return "" + obj;
-            }
-
-            if (typeof obj == "string") {
-                return '"' + obj + '"';
-            }
-
-            return stringifySomething(obj);
-        };
-
+        return "{" + enumeratedMethods.reduce(stringifyObjectProperty(source), "") + "}";
     }
-);
+
+    return function (obj) {
+        if (typeof obj == "undefined") {
+            return undefined;
+        }
+
+        if (typeof obj == "boolean" || typeof obj == "number") {
+            return "" + obj;
+        }
+
+        return stringifySomething(obj);
+    };
+
+});
